0b7d4d93f3a0dec616ae33d6af4e456cbce9234d
4r6h <hrahat023@gmail.com>
2022-08-03 17:06:13 +0000

    fixed paru install script

--- InstallParu.sh
+++ InstallParu.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-sudo pacman -S --noconfirm --needed git base base-devel;git clone https://aur.archlinux.org/paru.git;cd paru;makepkg -si --noconfirm
-echo"-------------------------------------------------------------------"
-echo"-------------------------------------------------------------------"
-echo"--------------------------Paru Installed---------------------------"
-echo"-------------------------------------------------------------------"
-echo"-------------------------------------------------------------------"
+sudo pacman -S --noconfirm --needed git base base-devel;git clone https://aur.archlinux.org/paru.git;cd paru;makepkg -si --noconfirm --needed
+echo"-------------------------------------"
+echo"-----------Paru Installed------------"
+echo"-------------------------------------"
 rm -rf paru
