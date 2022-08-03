61666f393c9c07f3b02711aef486e721aaa9e4bd
4r6h <hrahat023@gmail.com>
2022-08-03 19:37:36 +0000

    Added sddm files

--- set-rofi.sh
+++ set-rofi.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mkdir $HOME/.config/rofi/
-wget https://raw.githubusercontent.com/4r6h/myrofi/main/rofi/config.rasi;mv -u config.rasi $HOME/.config/rofi/
+https://raw.githubusercontent.com/4r6h/Dot4iles/main/myrofi/rofi/config.rasi;mv -u config.rasi $HOME/.config/rofi/
