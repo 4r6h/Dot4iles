88035dc8cdf12cc61647b2c957eec60a7c19a9a8
4r6h <hrahat023@gmail.com>
2022-08-03 19:52:01 +0000

    Finished the alacritty config

--- set-alacritty.sh
+++ set-alacritty.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mkdir $HOME/.config/alacritty/
-paru -S --needed --noconfirm alacritty;wget https://raw.githubusercontent.com/4r6h/AlacrittyConfig/main/alacritty.yml;wget https://raw.githubusercontent.com/dracula/alacritty/master/dracula.yml;mv -u alacritty.yml dracula.yml $HOME/.config/alacritty/
+paru -S --needed --noconfirm alacritty;wget https://raw.githubusercontent.com/4r6h/Dot4iles/main/AlacrittyConfig/alacritty.yml;wget https://raw.githubusercontent.com/dracula/alacritty/master/dracula.yml;mv -u alacritty.yml dracula.yml $HOME/.config/alacritty/
