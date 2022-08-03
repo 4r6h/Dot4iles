f9824d4cd8d72d8d5969e819045872c88e91424e
4r6h <hrahat023@gmail.com>
2022-08-03 19:47:38 +0000

    Update sddm & Alacritty configs

--- set-sddm.sh
+++ set-sddm.sh
@@ -2,8 +2,8 @@ paru -S --noconfirm sddm sddm-theme-sugar-candy-git
 wait
 sudo systemctl enable sddm
 wait
-wget https://raw.githubusercontent.com/4r6h/mysddm/main/sddm.conf
+wget https://raw.githubusercontent.com/4r6h/Dot4iles/main/mysddm/sddm.conf
 wait
-wget https://raw.githubusercontent.com/4r6h/mysddm/main/theme.conf
+wget https://raw.githubusercontent.com/4r6h/Dot4iles/main/mysddm/theme.conf
 wait
 sudo mv -u sddm.conf /etc/sddm.conf && sudo mv -u theme.conf /usr/share/sddm/themes/Sugar-Candy/theme.conf
