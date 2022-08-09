9da44973230074d893219a09af9f2cff5eede435
4r6h <hrahat023@gmail.com>
2022-08-09 07:02:28 +0000

    changed some options

--- z_100_testing/find_home_dir.sh
+++ z_100_testing/find_home_dir.sh
@@ -1,22 +1,55 @@
 #!/usr/bin/env bash
 
+while true 
+
+	do
+
 read -r -p " Please! Write your username : " un
+
+if id "$un" >/dev/null 2>&1; then
+
 #read -r -p " Please! Write your full name : " fn
 #read -r -p " Please! Write your home directory name : " hd
+
 homedir=$( getent passwd "$un" | cut -d: -f6 )
-while true 
-	do
-if [ ! -d $homedir ]; >/dev/null; then
-	echo "not found!!"
 
-elif [ -d $homedir ]; >/dev/null then
-	echo "not found!!"
+homedir=$(eval echo ~${un})
 
-elif [ -d $homedir ]; then
-	echo "found!!"
+home=$(echo ${homedir})
 
-else
-	echo "Check your username"
+#echo "$home"
+
+if [ $home = ~$un ]; then
+	echo "	username not found
+	Your Home Directory Should be in /home/$un
+	Please check it first then run the script"
+
+elif [ $home = "/home/$un" ]; then
+	echo "found"
+
+elif [ $un  = "root" ]; then
+	echo "you can't install this for root user"
+
+else [ $home = "/home/$un" ]; >/dev/null
+	echo "please check your username first"
 fi
-break
+else 
+	echo "User not found or you don't have a user in $un name"
+fi
+
+#if [ "$homedir" = "0" ]; then
+#else 
+#if [ ! -d $homedir ]; >/dev/null; then
+#	echo "not found!!"
+#
+#elif [ -d $homedir ]; >/dev/null then
+#	echo "not found!!"
+#
+#elif [ -d $homedir ]; then
+#	echo "found!!"
+#
+#else
+#	echo "Check your username"
+
+#fi
 	done
